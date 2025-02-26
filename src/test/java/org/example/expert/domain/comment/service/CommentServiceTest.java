c1f6514fcdc951c8964e21a413986743bfc46567
subbni <slfkak@naver.com>
2025-02-26 11:19:18 +0900

    Lv.3-2 CommentServiceTest 테스트 코드 수정

--- src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
+++ src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
@@ -5,6 +5,7 @@ import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.comment.repository.CommentRepository;
 import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.common.exception.ServerException;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
@@ -43,9 +44,10 @@ class CommentServiceTest {
         given(todoRepository.findById(anyLong())).willReturn(Optional.empty());
 
         // when
-        ServerException exception = assertThrows(ServerException.class, () -> {
-            commentService.saveComment(authUser, todoId, request);
-        });
+        InvalidRequestException exception = assertThrows (
+                InvalidRequestException.class,
+                () -> {commentService.saveComment(authUser, todoId, request);}
+        );
 
         // then
         assertEquals("Todo not found", exception.getMessage());
